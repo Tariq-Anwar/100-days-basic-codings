7282d05b3ee4877563bbe3b2ef164b0ee9add415
Tariq-Anwar <65457521+Tariq-Anwar@users.noreply.github.com>
2022-06-21 09:16:27 +0530

    First_N_Natural_No

---
+++ FirstNnatural_No.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int num;
+    cout << "Enter the number: "; cin >> num;
+    
+    for(int i=1;i<=num;i++){
+        printf("%d ",i);
+    }
+    
+    return 0;
+
+}
