cf335ca6b9276d93202705b5e6de7eb71ec0b322
Tariq-Anwar <65457521+Tariq-Anwar@users.noreply.github.com>
2022-06-22 08:55:02 +0530

    Add files via upload

---
+++ VoteUsingTrenaryOperator.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    int age;
+    cout <<"Enter your age: " ; cin >> age;
+    
+    age >= 18 ? cout << "Vote" : cout << "Can not Vote";
+    
+    return 0;
+}
